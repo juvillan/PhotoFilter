df6d785d8bee32931e1d8074140bbf75dd67efdf
Justin Villani <justinvillani@Justins-MacBook-Air.local>
2014-11-12 13:08:44 -0500

    Initial Commit

---
+++ PhotoFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoFilter
+//
+//  Created by Justin Villani on 11/12/14.
+//  Copyright (c) 2014 Justin Villani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
