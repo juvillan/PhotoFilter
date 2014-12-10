6bc4e0c4b641d5b1e4cb8dcd542f53ddc449c391
juvillan <juvillan@syr.edu>
2014-12-10 14:20:32 -0500

    Photo Filter App
    This app gives you the ability to have different photo filters on a
    picture of your chose.

--- PhotoFilter/ViewController.swift
+++ PhotoFilter/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //This is object of the image
+    @IBOutlet weak var photoimageview: UIImageView!
+    
+    //Create a place to render the image
+    let context = CIContext(options:nil);
+    
+    //this is an action created for the apply filter button
+    @IBAction func applyFilter(sender: AnyObject)
+    {
+        
+        //Create an image to filter
+        let InputImage = CIImage(image:photoimageview.image);
+        
+        //Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey:(Double(arc4random_uniform(314))/100)];
+        
+        //Apply a filter to the image
+        let filterImage = InputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor);
+        
+        //Render the filter image
+        let renderImage = context.createCGImage(filterImage, fromRect: filterImage.extent());
+        
+        //Reflect the change back in the interface
+        photoimageview.image = UIImage(CGImage: renderImage);
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
